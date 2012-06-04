7b0d6a34f0009c9042827f4dc3700f309d409a5e
JElchison <jelchison@gmail.com>
2012-06-03 21:49:23 -0400

    added support for passing server port as arg

--- Final/scripts/start.sh
+++ Final/scripts/start.sh
@@ -5,22 +5,26 @@ IWARS_MON=helios.ececs.uc.edu
 IWARS_MON_PORT=8160
 IWARS_SERVER_PORT=20000
 
+if [ $1 ]; then IWARS_SERVER_PORT=$1; fi
+
+echo "Using server port $IWARS_SERVER_PORT..."
+
 # avoid zombies.  ensure everything is stopped.
-./stop.sh
+`dirname $0`/stop.sh
 
 # tunnel for active client
-xterm -e ssh -L $IWARS_MON_PORT:$IWARS_MON:$IWARS_MON_PORT $IWARS_USER@$IWARS_MON &
+xterm -e ssh -vL $IWARS_MON_PORT:$IWARS_MON:$IWARS_MON_PORT $IWARS_USER@$IWARS_MON &
 echo "$!" > /tmp/iwars.ac.pid
 
 # tunnel for passive server
-xterm -e ssh -R $IWARS_SERVER_PORT:localhost:$IWARS_SERVER_PORT $IWARS_USER@$IWARS_MON &
+xterm -e ssh -vR $IWARS_SERVER_PORT:localhost:$IWARS_SERVER_PORT $IWARS_USER@$IWARS_MON &
 echo "$!" > /tmp/iwars.ps.pid
 
 # tunnel for RMI
-xterm -e ssh -L 1098:$IWARS_MON:1098 $IWARS_USER@$IWARS_MON &
+xterm -e ssh -vL 1098:$IWARS_MON:1098 $IWARS_USER@$IWARS_MON &
 echo "$!" > /tmp/iwars.rmi1.pid
 
 # tunnel for RMI
-xterm -e ssh -L 1099:localhost:1099 $IWARS_USER@$IWARS_MON &
+xterm -e ssh -vL 1099:localhost:1099 $IWARS_USER@$IWARS_MON &
 echo "$!" > /tmp/iwars.rmi2.pid
 
